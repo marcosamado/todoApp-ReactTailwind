1486cc5d37da7f84d2e1d3f991e4c0401b11422e
marcosamado <led_9dejulio@hotmail.com>
2023-08-31 18:37:18 -0300

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import Header from "./components/Header";
 import Main from "./components/Main/Main";
 
 const App = () => {
+    const darkTheme = "";
+    const lightTheme = "";
+
     return (
         <>
-            <div className="min-h-screen bg-gray-100 bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat dark:bg-gray-rgba dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] sm:bg-[url('./assets/images/bg-desktop-dark.jpg')]">
-                <Header />
-                <Main />
-                <Footer />
+            <div>
+                <div className="min-h-screen bg-gray-100 bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat dark:bg-gray-rgba dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] sm:bg-[url('./assets/images/bg-desktop-light.jpg')]">
+                    <Header />
+                    <Main />
+                    <Footer />
+                </div>
             </div>
         </>
     );
